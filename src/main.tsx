0a0100a24f355cd65a5470df5752d8e603c7b837
yashc2002 <yash.ch@navtech.io>
2026-03-26 16:48:08 +0530

    lovable watermark removed

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Remove Lovable watermark badge if injected into the DOM
+const removeLovableBadge = () => {
+  const selectors = [
+    '[data-lovable-badge]',
+    '.lovable-badge',
+    '#lovable-badge',
+    'a[href*="lovable.dev"][target="_blank"]',
+  ];
+  selectors.forEach((sel) => {
+    document.querySelectorAll(sel).forEach((el) => el.remove());
+  });
+};
+
+const observer = new MutationObserver(removeLovableBadge);
+observer.observe(document.body, { childList: true, subtree: true });
+removeLovableBadge();
+
 createRoot(document.getElementById("root")!).render(<App />);
